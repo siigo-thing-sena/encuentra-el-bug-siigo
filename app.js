4456e59830f4c956cc5674b01904ace72be3764c
danielsneira <danos.neira@gmail.com>
2021-10-22 08:02:08 -0500

    add globar variables and methods

--- app.js
+++ app.js
@@ -1,12 +1,164 @@
-import {} from 'dotenv/config'
-import {Server} from './models/server.js';
+import { } from 'dotenv/config'
+import { Server } from './models/server.js';
 
 const server = new Server();
-const turno = {number: 0};
-const jugadores = {number: 0};
+const turno = { number: 0 };
+const jugadores = { number: 0 };
 const rooms = []
 const secreto = []
+const cartas = [
+    {
+        id: 1,
+        nombre: "Pedro",
+        tipo: "Quien",
+        tachado: false,
+        imagen: '../assets/SENASOFT_IMG/img_personas/Pedro.png',
+    },
+
+    {
+        id: 2,
+        nombre: "Juan",
+        tipo: "Quien",
+        tachado: false,
+        imagen: '../assets/SENASOFT_IMG/img_personas/Juan.png',
+    },
+
+    {
+        id: 3,
+        nombre: "Carlos",
+        tipo: "Quien",
+        tachado: false,
+        imagen: '../assets/SENASOFT_IMG/img_personas/Carlos.png',
+    },
+
+    {
+        id: 4,
+        nombre: "Juanita",
+        tipo: "Quien",
+        tachado: false,
+        imagen: '../assets/SENASOFT_IMG/img_personas/Juanita.png',
+    },
+    {
+        id: 5,
+        nombre: "Antonio",
+        tipo: "Quien",
+        tachado: false,
+        imagen: '../assets/SENASOFT_IMG/img_personas/Antonio.png',
+    },
+
+    {
+        id: 6,
+        nombre: "Carolina",
+        tipo: "Quien",
+        tachado: false,
+        imagen: '../assets/SENASOFT_IMG/img_personas/Carlina.png',
+    },
+
+    {
+        id: 7,
+        nombre: "Manu",
+        tipo: "Quien",
+        tachado: false,
+        imagen: '../assets/SENASOFT_IMG/img_personas/Manu.png',
+    },
+
+    {
+        id: 8,
+        nombre: "Nomina",
+        tipo: "Modulo",
+        tachado: false,
+        imagen: '../assets/SENASOFT_IMG/img_modulos/Nomina.png',
+    },
+
+    {
+        id: 9,
+        nombre: "Facturación",
+        tipo: "Modulo",
+        tachado: false,
+        imagen: '../assets/SENASOFT_IMG/img_modulos/Facturación.png',
+    },
+
+    {
+        id: 10,
+        nombre: "Recibos",
+        tipo: "Modulo",
+        tachado: false,
+        imagen: '../assets/SENASOFT_IMG/img_modulos/Recibos.png',
+    },
+
+    {
+        id: 11,
+        nombre: "Comprobante contable",
+        tipo: "Modulo",
+        tachado: false,
+        imagen: '../assets/SENASOFT_IMG/img_modulos/Comprobantecontable.png',
+    },
+
+    {
+        id: 12,
+        nombre: "Usuarios",
+        tipo: "Modulo",
+        tachado: false,
+        imagen: '../assets/SENASOFT_IMG/img_modulos/Usuarios.png',
+    },
+
+    {
+        id: 13,
+        nombre: "Contabilidad",
+        tipo: "Modulo",
+        tachado: false,
+        imagen: '../assets/SENASOFT_IMG/img_modulos/Contabilidad.png',
+    },
+
+    {
+        id: 14,
+        nombre: "404",
+        tipo: "Error",
+        tachado: false,
+        imagen: '../assets/SENASOFT_IMG/img_errores/404.png',
+    },
+
+    {
+        id: 15,
+        nombre: "Stack overflow",
+        tipo: "Error",
+        tachado: false,
+        imagen: '../assets/SENASOFT_IMG/img_errores/Stackoverflow.png',
+    },
+
+    {
+        id: 16,
+        nombre: "Memory out of range",
+        tipo: "Error",
+        tachado: false,
+        imagen: '../assets/SENASOFT_IMG/img_errores/Memoryoutofrange.png',
+    },
+
+    {
+        id: 17,
+        nombre: "Null pointer",
+        tipo: "Error",
+        tachado: false,
+        imagen: '../assets/SENASOFT_IMG/img_errores/Nullpointer.png',
+    },
+
+    {
+        id: 18,
+        nombre: "Syntax error",
+        tipo: "Error",
+        tachado: false,
+        imagen: '../assets/SENASOFT_IMG/img_errores/Syntaxerror.png',
+    },
+
+    {
+        id: 19,
+        nombre: "Encoding error",
+        tipo: "Error",
+        tachado: false,
+        imagen: '../assets/SENASOFT_IMG/img_errores/Encodingerror.png',
+    },
+]
 
 server.listen();
 
-export {rooms, secreto, turno, jugadores}
+export { rooms, secreto, turno, jugadores, cartas }
--- sockets/controller.js
+++ sockets/controller.js
@@ -1,6 +1,10 @@
-import { rooms, secreto, turno, jugadores } from '../app.js'
+import { rooms, secreto, turno, jugadores, cartas } from '../app.js'
 
 var numClients = {};
+let j1,j2,j3,j4
+let cartasFiltradas
+
+const tipos = ["Error", "Modulo", "Quien"]
 
 const socketController = (socket) => {
   console.log("cliente conectado ", socket.id);
@@ -11,6 +15,10 @@ const socketController = (socket) => {
   // crear o unirse a sala
   socket.on('create-join-room', (data, callback) => {
     socket.join(data.room);
+    mezclarCartas()
+    crearSecreto()
+    filtrarSecreto()
+    repartirCartas()
     if (!rooms.includes(data.room)) {
       rooms.push(data.room)
     }
@@ -21,10 +29,20 @@ const socketController = (socket) => {
     }
     jugadores.number += data.jugador
     turno.number += data.jugador
+
+    let cartas = []
+
+    if (turno.number === 1){ cartas = j1 }
+    if (turno.number === 2){ cartas = j2 }
+    if (turno.number === 3){ cartas = j3 }
+    if (turno.number === 4){ cartas = j4 }
+
     let roomData = {
       rooms: rooms,
       jugadores: numClients[data.room],
-      turno: numClients[data.room]
+      turno: numClients[data.room],
+      secreto: secreto,
+      cartasJugador: cartas
     }
 
     callback(roomData)
@@ -39,5 +57,35 @@ const socketController = (socket) => {
   })
 
 };
+
+
+let mezclarCartas = () => {
+  cartas.sort(() => Math.random() - 0.5);
+}
+
+let crearSecreto = () => {
+  for (let i = 0; i < tipos.length; i++) {
+    let found = cartas.find(element => element.tipo === tipos[i]);
+    secreto.push(found)
+  }
+}
+
+let filtrarSecreto = () => {
+  cartasFiltradas = cartas
+  for (let i = 0; i < secreto.length; i++) {
+    cartasFiltradas = cartasFiltradas.filter((carta) => {
+      return carta.nombre !== secreto[i].nombre;
+    });
+    console.log(cartasFiltradas.length);
+  }
+  console.log(secreto.length);
+}
+
+let repartirCartas = () => {
+  j1 = cartasFiltradas.splice(0, cartasFiltradas.length / 4);
+  j2 = cartasFiltradas.splice(0, cartasFiltradas.length / 3);
+  j3 = cartasFiltradas.splice(0, cartasFiltradas.length / 2);
+  j4 = cartasFiltradas.splice(0, cartasFiltradas.length);
+}
 
 export { socketController };
